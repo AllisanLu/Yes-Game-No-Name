0ae4287f20a8af45ea1f857bc322dfd2d3d78353
Allison Lu <allison.lu@gatech.edu>
2022-07-15 15:27:36 -0400

    Create some cards

---
+++ Assets/Scripts/Cards/Deck.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    int level;
+    List<Card> cards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //make all the cards in a deck
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void shuffle()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Cards/Hand.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    List<Card> hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCard(Card card)
+    {
+
+    }
+
+    public void Clear()
+    {
+        hand = new List<Card>();
+    }
+}
